0b1e5f67d8b6c8c7b2996226880b751f10a96310
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-13 16:13:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let lado = parseFloat(prompt("Digite o valor do lado do quarado"))
+
+let calcLado = lado * lado
+let perimetro = lado * 4
+
+document.write(`O valor da area é igual a ${calcLado} e o valor do perimetro é igual a ${perimetro}`)
+
+console.log(calcLado)
+console.log(perimetro)
